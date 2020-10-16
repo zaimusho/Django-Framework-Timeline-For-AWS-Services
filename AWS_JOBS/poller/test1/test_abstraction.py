59f1ef5c98e2c80c89c0d0da821b3188a3f673fa
Nikkhil Butola <nikhilbutola33@gmail.com>
2020-10-16 11:42:42 +0530

    addon: junk.txt-testcase-for-test1/test_abstraction.py

--- AWS_JOBS/poller/test1/test_abstraction.py
+++ AWS_JOBS/poller/test1/test_abstraction.py
@@ -17,100 +17,57 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AWS_JOBS.settings")
 
 """
 
-# opposes the revoking of 3rd party django application
-import django
-django.setup()
-
 import boto3
-import bson
-from moto import mock_ec2
-# botocore has a client Stubber for mocking the error
-from botocore.stub import Stubber
-import sys, unittest
-from unittest.mock import patch, Mock, MagicMock
-from django.contrib.auth.models import User
-from django.test import TestCase, RequestFactory, Client
-from django.urls import reverse
-from mixer.backend.django import mixer
+import unittest
 from poller.abstraction import abstractionLayer
+from moto import mock_ec2
+from django.test import TestCase
 
-sys.modules["bson"] = Mock()
-sys.modules["boto3"] = Mock()
-
-
-class TestAbstraction(TestCase):
+class TestAbstractionSpinClientMethod(TestCase):
 
     def setUp(self):
-        self.client = Client()
-        self.boto3_mock = sys.modules["boto3"]
-        self.bson_mock = sys.modules["bson"].json_util
-        self.ec2_mock = "ec2"
-        self.health_mock = "health"
-        self.region_mock = "eu-central-1"
-        self.mock_roleArn = "mockArn:12345"
+        self.region = "us-east-2"
+        self.extern_service = "ec2"
+        self.temp_credentials = [
+            "mock_aws_access_key_id",
+            "mock_aws_secret_access_key",
+            "mock_aws_session_access_token"
+
+        ]
+
 
     @mock_ec2
+    def test_boto3_describe_regions(self):
+        ec2 = boto3.client("ec2", "us-east-1")
+        resp = ec2.describe_regions()
+        len(resp["Regions"]).should.be.greater_than(1)
+        for rec in resp["Regions"]:
+            rec["Endpoint"].should.contain(rec["RegionName"])
+
+        test_region = "us-east-1"
+        resp = ec2.describe_regions(RegionNames=[test_region])
+        resp["Regions"].should.have.length_of(1)
+        resp["Regions"][0].should.have.key("RegionName").which.should.equal(test_region)
+        resp["Regions"][0].should.have.key("OptInStatus").which.should.equal(
+            "opt-in-not-required"
+        )
+
+        test_region = "ap-east-1"
+        resp = ec2.describe_regions(RegionNames=[test_region])
+        resp["Regions"].should.have.length_of(1)
+        resp["Regions"][0].should.have.key("RegionName").which.should.equal(test_region)
+        resp["Regions"][0].should.have.key("OptInStatus").which.should.equal("not-opted-in")
+
+    
+    @mock_ec2
     def test_client_spin_status_check_try_catch(self):
-        
-        # abstraction_scan_region_mock.return_value = mock_scanned_regions
-        resources = boto3.resource(service_name="ec2", region_name="us-east-2")
-        instance = resources.instances,all()
+        objInstance = abstractionLayer(self.region)
+
+        conn = boto3.client("ec2", "us-east-1")
+        region = conn.describe_regions()
+        connect = conn.resource(self.extern_service, region_name=self.region)
+        scannedRegion = ["us-east-1", "us-east-2", "us-west-1"]
+        instance = connect.instances.all()
+        objInstance.clientSpinStatusCheck(self.extern_service, self.temp_credentials)
+        print(region)
         print(instance)
-
-        # instance = abstractionLayer(region)
-        # instance.clientSpinStatusCheck(mock_extern_service, )
-
-    # @patch("boto3.Instance")
-    # @patch("boto3.ServiceResource")
-    # @patch("poller.abstraction.abstractionLayer.scanRegion")
-    # @patch("boto3.Session")
-    # def test_client_spin_status_check_try_catch(self, session_mock, abstraction_scan_region_mock, resources_mock, instance_mock):
-        
-    #     region = self.region_mock
-    #     mock_extern_service = self.ec2_mock
-
-    #     mock_scanned_regions = {
-    #                         'Regions': [
-    #                             {
-    #                                 'Endpoint': 'mock_endpoint_1',
-    #                                 'RegionName': 'us-east-1',
-    #                             },
-    #                             {
-    #                                 'Endpoint': 'mock_endpoint_2',
-    #                                 'RegionName': 'us-east-2',
-    #                             },
-    #                             {
-    #                                 'Endpoint': 'mock_endpoint_3',
-    #                                 'RegionName': 'us-west-1',
-    #                             },
-    #                         ]
-    #                     }
-                    
-
-    #     mock_tmp_credentials = [
-    #         "mocked_aws_access_key_id",
-    #         "mocked_aws_access_key_id",
-    #         "mocked_aws_session_token",
-        
-    #     ]
-
-    #     mock_resource_instances = [
-    #             [
-    #                 {
-    #                 "instance_id": "mock_instance_id",
-    #                 "type": "mock_type",
-    #                 "platform": "mock_platform",
-    #                 "kernel_id": "mock_kernel_id",
-    #                 "hypervisor": "mock_hypervisor",
-    #                 "root_device_name": "mock_device_name"
-
-    #             },
-    #         ]    
-    #     ]
-
-    #     abstraction_scan_region_mock.return_value = mock_scanned_regions
-    #     resources_mock.return_value = mock_resource_instances
-    #     # instance_mock.return_value = mock_resource_instances
-    #     print(resources_mock)
-    #     instance = abstractionLayer(region)
-    #     instance.clientSpinStatusCheck(mock_extern_service, mock_tmp_credentials)
